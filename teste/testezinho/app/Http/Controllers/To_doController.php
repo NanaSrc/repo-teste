80868e1a11b42a5e5121cdb63f0c41ca82324386
Anna <annascwo@gmail.com>
2021-04-21 14:58:22 +0100

    Ligação a BD

---
+++ teste/testezinho/app/Http/Controllers/To_doController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\To_do;
+use Illuminate\Http\Request;
+
+class To_doController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\To_do  $to_do
+     * @return \Illuminate\Http\Response
+     */
+    public function show(To_do $to_do)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\To_do  $to_do
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(To_do $to_do)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\To_do  $to_do
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, To_do $to_do)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\To_do  $to_do
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(To_do $to_do)
+    {
+        //
+    }
+}
